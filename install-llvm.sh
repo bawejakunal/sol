e2df142331dc8c6c03739aa0d7595c8ea1ba315e
Kunal Baweja <bawejakunal15@gmail.com>
2017-10-28 19:38:54 -0400

    yes flag llvm install script

--- install-llvm.sh
+++ install-llvm.sh
@@ -4,10 +4,10 @@
 sudo apt update
 
 # install llvm runtime and m4 preprocessor
-sudo apt install llvm-3.8-dev llvm-3.8 llvm-runtime m4
+sudo apt install --yes llvm-3.8-dev llvm-3.8 llvm-runtime m4
 
 # install ocaml llvm binding
-opam install llvm.3.8
+opam install --yes llvm.3.8
 
 # set opam environment for current shell
 eval `opam config env`
