f9d97f9b444ead173beab5e068f8f5eda3bcec73
Kunal Baweja <bawejakunal15@gmail.com>
2017-11-04 16:05:17 -0400

    gold tests

--- tests/test-add.sol
+++ tests/test-add.sol
@@ -1,10 +1,9 @@
 func int add(int x, int y)
 {
-  return x + y;
+    return x + y;
 }
 
 func main()
 {
-  console print( add(17, 25) );
-  return 0;
+    consolePrint(toString(add(17, 25)));
 }
---
+++ tests/test-hello.sol
@@ -0,0 +1,3 @@
+func main() {
+	consolePrint("Hello World");
+}
