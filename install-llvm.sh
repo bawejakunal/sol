c7cd2c8e769196c17ea9e4e6c50c88868f74d8ac
Kunal Baweja <bawejakunal15@gmail.com>
2017-11-16 18:24:18 -0500

    update sdl test scripts

--- install-llvm.sh
+++ install-llvm.sh
@@ -6,6 +6,9 @@ sudo apt update
 # install llvm runtime and m4 preprocessor
 sudo apt install --yes llvm-3.8-dev llvm-3.8 llvm-runtime m4 llvm
 
+# install sdl
+sudo apt install --yes sdl2-2.0 libsdl2-dev
+
 # install ocaml llvm binding
 opam install --yes llvm.3.8
 
