d662807ad9f6caa90c3ef12886c3f6a1a4fa6a56
Kunal Baweja <bawejakunal15@gmail.com>
2017-11-16 18:45:31 -0500

    add sdl dependencies

--- install-llvm.sh
+++ install-llvm.sh
@@ -7,7 +7,7 @@ sudo apt update
 sudo apt install --yes llvm-3.8-dev llvm-3.8 llvm-runtime m4 llvm
 
 # install sdl
-sudo apt install --yes sdl2-2.0 libsdl2-dev
+sudo apt install --yes libegl1-mesa-dev libegl2-mesa-dev sdl2-2.0 libsdl2-dev
 
 # install ocaml llvm binding
 opam install --yes llvm.3.8
