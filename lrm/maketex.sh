04f0d01394ccc37b89eae90ac2035af5c0dbf4a2
Kunal Baweja <bawejakunal15@gmail.com>
2017-12-19 13:42:40 -0500

    update tutorial in report

--- lrm/maketex.sh
+++ lrm/maketex.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 # Compile twice for content table correctness
-pdflatex -synctex=1 -interaction=nonstopmode --shell-escape $1 &&
 pdflatex -synctex=1 -interaction=nonstopmode --shell-escape $1
