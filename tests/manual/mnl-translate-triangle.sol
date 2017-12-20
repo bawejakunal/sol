e658dba1f7fafbf701612c766f5fad3035805321
DyerSituation <ead2174@columbia.edu>
2017-12-20 13:27:42 -0500

    basic translate testing

---
+++ tests/manual/mnl-translate-triangle.sol
@@ -0,0 +1,47 @@
+/*@author: Erik Dyer & Kunal Baweja */
+
+/* Test Triangle*/
+/* Run this code, */
+
+
+func findCenter(int [2]m, int[2]x, int[2]y) {
+    m[0] = (x[0] + y[0]) / 2;
+    m[1] = (x[1] + y[1]) / 2;
+}
+
+shape Triangle {
+    int[2] a;
+    int[2] b;
+    int[2] c;
+
+    int[2] abm;
+    int[2] bcm;
+    int[2] acm;
+
+    construct (int[2] a_init, int[2] b_init, int[2] c_init){
+        a = a_init;
+        b = b_init;
+        c = c_init;
+
+        findCenter(abm, a, b);
+        findCenter(acm, a, c);
+        findCenter(bcm, c, b); 
+    }
+
+    draw() {
+        /* Draw lines between the three vertices of the triangle*/
+        drawCurve(a, abm, b, 2, [150, 100, 0]);
+        drawCurve(b, bcm, c, 2, [0, 150, 100]);
+        drawCurve(c, acm, a, 2, [100, 0, 150]);
+    }
+}
+
+func main(){
+	Triangle t;
+	t = shape Triangle([170, 340], [470, 340], [320, 140]);
+    t.render = {
+        translate([130, 130], 2);
+        translate([-30, -130], 3);
+        translate([-100, -100], 2);
+    }
+}
